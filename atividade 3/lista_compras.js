1a5b284be930914cbd6f7571cf2bbf69bdf6d4cd
Caio <>
2026-03-12 12:15:44 -0300

    array

---
+++ atividade 3/lista_compras.js
@@ -0,0 +1,13 @@
+let lista_compras = ["Arroz", "Feijão", "Açúcar", "Salmão"];
+lista_compras.push("Macarrão")
+lista_compras.push("Macarrão")
+lista_compras.push("Macarrão")
+lista_compras.push("Macarrão")
+  console.log(item);
+});
+
+let cesta_frutas = ["Maçã", "Manga", "Uva", "Limão"];
+cesta_frutas.push("Mamão" , "Pêssego");
+cesta_frutas.forEach(fruta => {
+    console.log(fruta);
+});
